05afca1d8669b27905e9caae404b63a02d309d75
Sakshi <sakshi.kapoor1992@gmail.com>
2017-03-14 14:32:48 +0530

    add a loop

---
+++ app/src/main/java/com/example/sakshi/mynav/Demo.java
@@ -0,0 +1,17 @@
+package com.example.sakshi.mynav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+        for(int i=0;i<10;i++)
+        {
+
+        }
+    }
+}
